a31455c09d43ecfa95bedfcdd38d64f0a32076d1
elisno <elisno@users.noreply.github.com>
2022-09-22 21:23:08 +0000

    deploy: cleanlab/cleanlab@28446e949eeb46c3e8debf719d1381d90ca1ecc4

--- versioning.js
+++ versioning.js
@@ -1,11 +1,4 @@
 var Version = {
   version_number: "v2.1.0",
-  commit_hash: "f9039e63e303bda2e6251e283ac85e1915c428a4",
+  commit_hash: "28446e949eeb46c3e8debf719d1381d90ca1ecc4",
 };
-
-// Add new version tags here before a release 
-var ReleaseVersions = [
-  "2.1.0",
-  "2.0.0",
-  "1.0.1",
-]
