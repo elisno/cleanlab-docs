85dfae5167572aceb66901fb5973b3611d9cdd2e
elisno <elisno@users.noreply.github.com>
2024-02-27 10:39:56 +0000

    deploy: elisno/cleanlab@9d740b015f2b550b7eccbda36c140369fece63b6

--- versioning.js
+++ versioning.js
@@ -1,4 +1,4 @@
 var Version = {
-  version_number: "placeholder_version_number",
-  commit_hash: "e5ea09b4e09144c4027ee8b3bf56ec2f381b71ba",
+  version_number: "v2.6.1-alpha1",
+  commit_hash: "9d740b015f2b550b7eccbda36c140369fece63b6",
 };
